2e5394487986057c87109cfdcfc720e22190a15c
Alexander1411 <124513544+Alexander1411@users.noreply.github.com>
2024-05-06 13:49:20 +0100

    Update manage.py

--- inventory_management-master/inventory_management/manage.py
+++ inventory_management-master/inventory_management/manage.py
@@ -1,3 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")#URL leading to method
+def hello(): # Name of the method
+ return("Hello World!") #indent this line
+if __name__ == "__main__":
+ app.run(host='0.0.0.0', port='8080') # indent this line
+
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
